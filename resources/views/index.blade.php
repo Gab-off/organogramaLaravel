c1be2e8f55d4103ad6ddf8aee849beddb5596fe0
Gab-off <albuquerque1.gabriel@gmail.com>
2025-10-01 22:59:41 -0300

    First commit for this project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Página principal')
+
+@section('content')
+    <h1>Welcome to the Home Page</h1>
+@endsection
+
+@section('footer')
+    <footer>
+        This is a footer of page
+    </footer>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header class="flex justify-between gap-20 items-center">
+    <h1 class="font-bold text-3xl uppercase text-blue-950">Organograma</h1>
+    <ul class="flex gap-4 *:bg-blue-900 *:hover:bg-blue-300 *:hover:text-blue-900 *:transition text-blue-50 *:text-balance *:rounded-2xl *:px-2 *:py-1 text-center *:content-center">
+        <a href="">
+            <li>Coordenadoria de obras</li>
+        </a>
+        <a href="">
+            <li>Coordenadoria de projetos</li>
+        </a>
+        <a href="">
+            <li>Coordenadoria da usina de asfalto</li>
+        </a>
+        <a href="">
+            <li>Coordenadoria de manutenção</li>
+        </a>
+        <a href="">
+            <li>Coordenadoria jurídica</li>
+        </a>
+        <a href="">
+            <li>Coordenadoria administrativa financeira</li>
+        </a>
+    </ul>
+</header>
